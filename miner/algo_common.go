7aa31f63f7ea1b3b0c732fc33c537fc05657482f
Niclas Blomberg <nicke.blomberg@gmail.com>
2023-08-03 12:35:27 +0200

    remove nonce subtraction

--- miner/algo_common.go
+++ miner/algo_common.go
@@ -438,7 +438,7 @@ func (envDiff *environmentDiff) commitPayoutTx(amount *big.Int, sender, receiver
 	signer := envDiff.baseEnvironment.signer
 	tx, err := types.SignNewTx(prv, signer, &types.DynamicFeeTx{
 		ChainID:   chData.chainConfig.ChainID,
-		Nonce:     envDiff.state.GetNonce(sender) - 1,
+		Nonce:     envDiff.state.GetNonce(sender),
 		GasTipCap: new(big.Int),
 		GasFeeCap: envDiff.header.BaseFee,
 		Gas:       gas,
